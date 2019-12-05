5e8719afd65ee31911d2ff8eb18b5f5e2294a35c
Steffen Zschaler <steffen.zschaler@kcl.ac.uk>
2019-12-05 13:41:18 +0000

    Package documentation added

---
+++ uk.ac.kcl.inf.szschaler.minesweeper/src/uk/ac/kcl/inf/szschaler/minesweeper/model/cells/inheritance_based/package-info.java
@@ -0,0 +1,6 @@
+/**
+ * This package contains a cell implementation that is inheritance-based.
+ * 
+ * @author Steffen Zschaler
+ */
+package uk.ac.kcl.inf.szschaler.minesweeper.model.cells.inheritance_based;
